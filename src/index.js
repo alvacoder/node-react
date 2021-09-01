f0e88dd407811781bc7046cae4d6f8f374d41d77
alvacoder <idrisadeniji01@gmail.com>
2021-09-01 20:24:37 +0100

    app setup complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import express from "express";
 
 const app = express();
 const port = process.env.PORT || 7001;
+const path = __dirname + '/server/views/';
 
+app.use(express.static(path));
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
 
 app.listen(port, () => {
     console.log('app running on port %s', port);
